d25cf615a5a1674beb37db6d43d034047e64a9bc
tommmb <86568741+tommmb@users.noreply.github.com>
2022-01-05 10:45:17 +0000

    Update account.py

--- account.py
+++ account.py
@@ -4,3 +4,35 @@ class Account:
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
+        
+# class Account:
+#     def __init__(self, id, email, first_name, last_name):
+#         self._id = id
+#         self._email = email
+#         self._first_name = first_name
+#         self._last_name = last_name
+        
+#     def get_id(self):
+#         return self._id
+    
+#     def get_email(self):
+#         return self._email
+    
+#     def get_first_name(self):
+#         return self._first_name
+    
+#     def get_last_name(self):
+#         return self._last_name
+    
+#     def set_id(self, id):
+#         self._id = id
+    
+#     def set_email(self, email):
+#         self._email = email
+    
+#     def set_first_name(self, first_name):
+#         self._first_name = first_name
+    
+#     def set_last_name(self, last_name):
+#         self._last_name = last_name
+        
